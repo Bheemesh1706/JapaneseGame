e23749b2ab899f3c6035092a73f5f707a0c18c3b
bheemesh1706 <bheemeshbala.71@gmail.com>
2023-06-21 13:17:12 +0530

    Physics setup done and added a model

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { Experience } from "./components/Experience";
 import { Physics } from "@react-three/rapier";
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
+    <Canvas shadows camera={{ position: [0, 6, 14], fov: 42 }}>
+      <color attach="background" args={["#dbecfb"]} />
       <Physics>
         <Experience />
       </Physics>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,27 @@
-import { OrbitControls } from "@react-three/drei";
+import { Cylinder, OrbitControls } from "@react-three/drei";
+import { CylinderCollider, RigidBody } from "@react-three/rapier";
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <meshNormalMaterial />
-        <boxBufferGeometry />
-      </mesh>
+      {/*Lights*/}
+      <ambientLight intensity={1} />
+      <directionalLight
+        position={[5, 5, 5]}
+        intensity={0.8}
+        castShadow
+        color={"9e69da"}
+      />
+
+      {/*Stage*/}
+      <RigidBody colliders={false} type="fixed" position-y={-0.5}>
+        <CylinderCollider args={[1/2, 5]} />
+        <Cylinder scale={[5,1,5]} receiveShadow>
+          <meshStandardMaterial color={"white"} />
+        </Cylinder>
+      </RigidBody>
     </>
   );
 };
+
